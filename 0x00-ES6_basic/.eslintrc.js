6bc491200f0e3ed6286c3b3ccf8504ef9f098541
josmuiruri <josemuiruri51@gmail.com>
2024-05-28 21:20:49 +0300

    using an arrow function

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		jest: true,
-	},
-	extends: 'eslint:recommended',
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	rules: {
-		'no-var': 'error',
-		'prefer-const': 'error',
-	},
+   env: {
+	   browser: true,
+	   es2021: true,
+	   jest: true,
+   },
+   extends: 'eslint:recommended',
+   parserOptions: {
+	ecmaVersion: 12,
+	sourceType: 'module',
+   },
+   rules: {
+	'no-var': 'error',
+	'prefer-const': 'error',
+	'no-tabs': 'error',
+	'indent': ['error', 2],
+   },
 };
