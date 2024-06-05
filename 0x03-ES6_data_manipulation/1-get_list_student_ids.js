ef40def4febcc5fd1bf517058c3c97d10163b184
josmuiruri <josemuiruri51@gmail.com>
2024-06-05 05:59:50 +0300

    added an example

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,14 @@ function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
   }
-  return students.map(student => student.id);
+  return students.map((student) => student.id);
 }
+
+const students = [
+  { id: 1, firstName: "Guillaume", location: "San Francisco" },
+  { id: 2, firstName: "james", location: "Columbia" },
+  { id: 5, firstName: "Serena", location: "San Francisco" },
+];
+
+console.log(getListStudentIds(students));
+console.log(getListStudentIds("teacher"));
