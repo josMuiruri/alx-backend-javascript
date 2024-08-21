bbeca9332d5fdb3847311754afa19ec3d1243afe
josMuiruri <josemuiruri51@gmail.com>
2024-08-21 15:51:21 +0300

    pass Lint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,9 @@
 const http = require('http');
+
 const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, '127.0.01', () => {
-  console.log('Hello Holberton School!');
-});
+app.listen(1245, '127.0.01', () => {});
 
 module.exports = app;
