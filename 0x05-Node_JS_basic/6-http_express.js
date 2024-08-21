9e3e70fd2e1b43e6e0c8de184e2649823feeb669
josMuiruri <josemuiruri51@gmail.com>
2024-08-21 18:27:26 +0300

    Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+const port = 1245;
+app.listen(port, () => {});
+
+module.exports = app;
