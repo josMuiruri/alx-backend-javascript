55e98017fb86b7b2c9abf51ad12a63f8e466b387
josMuiruri <josemuiruri51@gmail.com>
2024-08-21 18:32:23 +0300

    moved the port to the top

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express;
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello Holberton School!');
 });
-const port = 1245;
+
 app.listen(port, () => {});
 
 module.exports = app;
